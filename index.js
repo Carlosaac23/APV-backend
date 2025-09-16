a62152bba0b39dfe37221b6351b9d5e8ce04d404
Carlos <carlosaac232001@gmail.com>
2025-09-16 15:42:18 -0500

    feat: add and listen main server

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('Desde Node.js');
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+import router from './routes/veterinarianRoutes.js';
+
+const app = express();
+app.use(express.json());
+dotenv.config();
+connectDB();
+
+app.use('/api/veterinarians', router);
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor funcionando en el puerto ${PORT}`);
+});
