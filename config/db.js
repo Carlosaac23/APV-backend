801d9094023c003af969dbb69a3c59ed5b36c83b
Carlos <carlosaac232001@gmail.com>
2025-09-16 14:25:32 -0500

    feat: add MongoDB connection

---
+++ config/db.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+  const mongoUri = process.env.MONGO_URI;
+
+  try {
+    const db = await mongoose.connect(mongoUri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    const url = `${db.connection.host}:${db.connection.port}`;
+    console.log(`MongoDB conectado en: ${url}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
