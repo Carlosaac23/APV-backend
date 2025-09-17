4277aacfe40fc7f84205ffc3f7b63288800004c2
Carlos <carlosaac232001@gmail.com>
2025-09-17 12:55:36 -0500

    feat: add authenticate function auth user by email and password using JWT

--- controllers/veterinarianController.js
+++ controllers/veterinarianController.js
@@ -1,11 +1,11 @@
 import Veterinarian from '../models/Veterinarian.js';
+import generateJWT from '../helpers/generateJWT.js';
 
 async function register(req, res) {
   const { email } = req.body;
 
   // Prevenir usuarios duplicados
   const userExist = await Veterinarian.findOne({ email });
-
   if (userExist) {
     const error = new Error('Usuario ya registrado');
     return res.status(400).json({ msg: error.message });
@@ -14,6 +14,7 @@ async function register(req, res) {
   try {
     const veterinarian = new Veterinarian(req.body);
     const savedVeterinarian = await veterinarian.save();
+
     res.json(savedVeterinarian);
   } catch (error) {
     console.error(error);
@@ -30,7 +31,6 @@ async function verify(req, res) {
   const { token } = req.params;
 
   const verifyUser = await Veterinarian.findOne({ token });
-
   if (!verifyUser) {
     const error = new Error('Token no válido');
     return res.status(404).json({ msg: error.message });
@@ -47,4 +47,30 @@ async function verify(req, res) {
   }
 }
 
-export { register, profile, verify };
+async function authenticate(req, res) {
+  const { email, password } = req.body;
+
+  // Comprobar si el usuario existe
+  const user = await Veterinarian.findOne({ email });
+  if (!user) {
+    const error = new Error('El usuario no existe');
+    return res.status(403).json({ msg: error.message });
+  }
+
+  // Comprobar si el usuario está verificado
+  if (!user.verify) {
+    const error = new Error('La cuenta no está verificada');
+    return res.status(403).json({ msg: error.message });
+  }
+
+  // Revisar el password
+  if (!(await user.verifyPassword(password))) {
+    const error = new Error('La contraseña es incorrecta');
+    return res.status(403).json({ msg: error.message });
+  }
+
+  // Autenticar
+  res.json({ token: generateJWT(user.id) });
+}
+
+export { register, profile, verify, authenticate };
