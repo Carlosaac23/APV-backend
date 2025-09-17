a552b2a8444b94e14fa58d16b3e2812e54ffb2ef
Carlos <carlosaac232001@gmail.com>
2025-09-17 12:56:59 -0500

    feat: add middleware to profile route

--- routes/veterinarianRoutes.js
+++ routes/veterinarianRoutes.js
@@ -3,12 +3,16 @@ import {
   register,
   profile,
   verify,
+  authenticate,
 } from '../controllers/veterinarianController.js';
+import checkAuth from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/', register);
-router.get('/profile', profile);
 router.get('/verify/:token', verify);
+router.post('/login', authenticate);
+
+router.get('/profile', checkAuth, profile);
 
 export default router;
