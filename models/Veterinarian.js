d2c8d8d2f156827fdd4a2c4ed5d9ef892bc5b7ac
Carlos <carlosaac232001@gmail.com>
2025-09-17 12:57:29 -0500

    feat: add hash method to hash user password

--- models/Veterinarian.js
+++ models/Veterinarian.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 import generateToken from '../helpers/generateToken.js';
 
 const veterinarianSchema = mongoose.Schema({
@@ -35,6 +36,19 @@ const veterinarianSchema = mongoose.Schema({
     default: false,
   },
 });
+
+veterinarianSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+veterinarianSchema.methods.verifyPassword = async function (formPassword) {
+  return await bcrypt.compare(formPassword, this.password);
+};
 
 const Veterinarian = mongoose.model('Veterinarian', veterinarianSchema);
 export default Veterinarian;
