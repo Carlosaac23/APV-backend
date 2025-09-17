9e56ff3dd12844dfcfca2a87ca5e26cb1d7e1242
Carlos <carlosaac232001@gmail.com>
2025-09-17 11:26:42 -0500

    feat: add function to prevent register users with same email and verify function to verify new user

--- controllers/veterinarianController.js
+++ controllers/veterinarianController.js
@@ -1,7 +1,15 @@
 import Veterinarian from '../models/Veterinarian.js';
 
 async function register(req, res) {
-  // const { name, email, password } = req.body;
+  const { email } = req.body;
+
+  // Prevenir usuarios duplicados
+  const userExist = await Veterinarian.findOne({ email });
+
+  if (userExist) {
+    const error = new Error('Usuario ya registrado');
+    return res.status(400).json({ msg: error.message });
+  }
 
   try {
     const veterinarian = new Veterinarian(req.body);
@@ -18,4 +26,25 @@ function profile(req, res) {
   });
 }
 
-export { register, profile };
+async function verify(req, res) {
+  const { token } = req.params;
+
+  const verifyUser = await Veterinarian.findOne({ token });
+
+  if (!verifyUser) {
+    const error = new Error('Token no válido');
+    return res.status(404).json({ msg: error.message });
+  }
+
+  try {
+    verifyUser.token = null;
+    verifyUser.verify = true;
+    await verifyUser.save();
+
+    res.json({ msg: 'Usuario Confirmado Correctamente' });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export { register, profile, verify };
