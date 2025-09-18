17b186ab022fb2b4f4aa13eaabf9f498f2def83b
Carlos <carlosaac232001@gmail.com>
2025-09-18 09:50:18 -0500

    feat: create checkAuth function to validate user by token and let him see profile page

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,12 +1,29 @@
-export default function checkAuth(req, res, next) {
-  const { authorization: token } = req.headers;
+import jwt from 'jsonwebtoken';
+import Veterinarian from '../models/Veterinarian.js';
+
+export default async function checkAuth(req, res, next) {
+  let { authorization: token } = req.headers;
+  const { JWT_SECRET } = process.env;
 
   if (token && token.startsWith('Bearer')) {
-    console.log('Sí tiene el token con Bearer');
+    try {
+      token = req.headers.authorization.split(' ')[1];
+      const decoded = jwt.verify(token, JWT_SECRET);
+      req.veterinarian = await Veterinarian.findById(decoded.id).select(
+        '-password -token -verify'
+      );
+
+      return next();
+    } catch (error) {
+      const e = new Error('Token no válido');
+      return res.status(403).json({ msg: e.message });
+    }
   }
 
-  const error = new Error('Token no válido o inexistente');
-  res.status(403).json({ msg: error.message });
+  if (!token) {
+    const error = new Error('Token no válido o inexistente');
+    res.status(403).json({ msg: error.message });
+  }
 
   next();
 }
