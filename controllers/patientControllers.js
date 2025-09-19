26462307fe3b80f477bc23a0a835afa2fd234bb0
Carlos <carlosaac232001@gmail.com>
2025-09-18 21:06:28 -0500

    feat: - create getPatients function to get all patients from one veterinarian - create getPatient to get one patient from one veterinarian - create updatePatient to update a patient by id and only by its veterinarian - create deletePatient to delete a patient only by its veterinarian

--- controllers/patientControllers.js
+++ controllers/patientControllers.js
@@ -12,6 +12,77 @@ async function addPatient(req, res) {
   }
 }
 
-function getPatients(req, res) {}
+async function getPatients(req, res) {
+  const patients = await Patient.find()
+    .where('veterinarian')
+    .equals(req.veterinarian);
 
-export { addPatient, getPatients };
+  res.json(patients);
+}
+
+async function getPatient(req, res) {
+  const { id } = req.params;
+  const patient = await Patient.findById(id);
+
+  if (!patient) {
+    const error = new Error('Paciente No Encontrado');
+    return res.status(404).json({ msg: error.message });
+  }
+
+  if (String(patient.veterinarian._id) !== String(req.veterinarian._id)) {
+    return res.json({ msg: 'Acción No Válida' });
+  }
+
+  res.json(patient);
+}
+
+async function updatePatient(req, res) {
+  const { id } = req.params;
+  const patient = await Patient.findById(id);
+
+  if (!patient) {
+    const error = new Error('Paciente No Encontrado');
+    return res.status(404).json({ msg: error.message });
+  }
+
+  if (String(patient.veterinarian._id) !== String(req.veterinarian._id)) {
+    return res.json({ msg: 'Acción No Válida' });
+  }
+
+  // Actualizar paciente
+  patient.name = req.body.name || patient.name;
+  patient.owner = req.body.owner || patient.owner;
+  patient.email = req.body.email || patient.email;
+  patient.date = req.body.date || patient.date;
+  patient.sympstoms = req.body.sympstoms || patient.sympstoms;
+
+  try {
+    const updatedPatient = await patient.save();
+    res.json(updatedPatient);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function deletePatient(req, res) {
+  const { id } = req.params;
+  const patient = await Patient.findById(id);
+
+  if (!patient) {
+    const error = new Error('Paciente No Encontrado');
+    return res.status(404).json({ msg: error.message });
+  }
+
+  if (String(patient.veterinarian._id) !== String(req.veterinarian._id)) {
+    return res.json({ msg: 'Acción No Válida' });
+  }
+
+  try {
+    await patient.deleteOne();
+    res.json({ msg: 'Paciente Eliminado' });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export { addPatient, getPatients, getPatient, updatePatient, deletePatient };
