5b7a80823bc9758e8a9154f69f07ca9df9ebc27c
Carlos <carlosaac232001@gmail.com>
2025-10-03 23:52:30 -0500

    fix: update TS path files

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import type { CorsOptions } from 'cors';
-import connectDB from './config/db.js';
+import connectDB from './config/db.ts';
 import veterinarianRoutes from './routes/veterinarianRoutes.ts';
 import patientRoutes from './routes/patientRoutes.ts';
 
