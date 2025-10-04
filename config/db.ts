5b4bdc7a097413b61889413cdefafa92ed5b3372
Carlos <carlosaac232001@gmail.com>
2025-10-04 00:11:21 -0500

    fix: add types and TS default imports

--- config/db.ts
+++ config/db.ts
@@ -1,14 +1,14 @@
-import mongoose from 'mongoose';
+import { connect } from 'mongoose';
 
 const connectDB = async () => {
   const { MONGO_URI } = process.env;
 
   try {
-    const db = await mongoose.connect(MONGO_URI);
+    const db = await connect(MONGO_URI!);
 
     const url = `${db.connection.host}:${db.connection.port}`;
     console.log(`MongoDB conectado en: ${url}`);
-  } catch (error) {
+  } catch (error: any) {
     console.error(`Error: ${error.message}`);
     process.exit(1);
   }
