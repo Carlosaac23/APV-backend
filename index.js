e3d036d14d01c31554f0f600fff1ef227005b804
Carlos <carlosaac232001@gmail.com>
2026-01-22 23:21:30 -0500

    chore: move from ts to js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import type { CorsOptions } from 'cors';
+
 import connectDB from './config/db.ts';
 import veterinarianRoutes from './routes/veterinarianRoutes.ts';
 import patientRoutes from './routes/patientRoutes.ts';
@@ -13,7 +13,7 @@ connectDB();
 
 const { FRONTEND_URL } = process.env;
 const allowedDomains = [FRONTEND_URL];
-const corsOptions: CorsOptions = {
+const corsOptions = {
   origin: function (origin, callback) {
     if (allowedDomains.indexOf(origin) !== -1) {
       callback(null, true);
