7ab48d934c0853bd333d163c9cdafdc380d653c7
Carlos <carlosaac232001@gmail.com>
2026-01-22 23:21:47 -0500

    chore: delete unuse files

--- types/express/index.d.ts
+++
@@ -1,9 +0,0 @@
-import type { IVeterinarian } from '../../models/Veterinarian';
-
-declare global {
-  namespace Express {
-    export interface Request {
-      veterinarian?: IVeterinarian;
-    }
-  }
-}
