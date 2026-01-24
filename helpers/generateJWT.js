5d23ddc19d0e4f2917fdadabda067a3770536d2b
Carlos <carlosaac232001@gmail.com>
2026-01-24 10:40:25 -0500

    refactor: delete all files

--- helpers/generateJWT.js
+++
@@ -1,8 +0,0 @@
-import jwt from 'jsonwebtoken';
-
-export default function generateJWT(id) {
-  const { JWT_SECRET } = process.env;
-  return jwt.sign({ id }, JWT_SECRET, {
-    expiresIn: '30 days',
-  });
-}
--- helpers/generateToken.js
+++
@@ -1,3 +0,0 @@
-export default function generateToken() {
-  return Date.now().toString(32) + Math.random().toString(32).substring(2);
-}
