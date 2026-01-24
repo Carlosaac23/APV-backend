c33365ede1b60dcac16cbbd1fe31e6a87b83be61
Carlos <carlosaac232001@gmail.com>
2026-01-24 10:40:08 -0500

    feat: create index file

--- index.js
+++ index.js
@@ -1,35 +1,7 @@
-import cors from 'cors';
-import dotenv from 'dotenv';
 import express from 'express';
 
-import connectDB from './config/db.js';
-import patientRoutes from './routes/patientRoutes.js';
-import veterinarianRoutes from './routes/veterinarianRoutes.js';
-
 const app = express();
-app.use(express.json());
-dotenv.config();
-connectDB();
 
-const { FRONTEND_URL } = process.env;
-const allowedDomains = [FRONTEND_URL];
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (allowedDomains.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('No permitido por CORS'));
-    }
-  },
-};
-
-app.use(cors(corsOptions));
-
-app.use('/api/veterinarians', veterinarianRoutes);
-app.use('/api/patients', patientRoutes);
-
-const PORT = process.env.PORT || 4000;
-
-app.listen(PORT, () => {
-  console.log(`Servidor funcionando en el puerto ${PORT}`);
+app.listen(4000, () => {
+  console.log('Servidor funcionando en el puerto 4000');
 });
