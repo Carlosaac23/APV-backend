06231a8ca8c6536f34a4ddbe98551b6e42d7f2b5
Carlos <carlosaac232001@gmail.com>
2026-01-24 11:54:43 -0500

    feat: create first register controller to add a new veterinarian

--- controllers/veterinarianController.js
+++ controllers/veterinarianController.js
@@ -1,3 +1,22 @@
-export const register = (req, res) => {
-  res.json({ msg: 'Registrando usuario...' });
+import Veterinarian from '../models/Veterinarian.js';
+
+export const register = async (req, res) => {
+  const { email } = req.body;
+
+  // Prevent duplicate users
+  const isEmailTaken = await Veterinarian.findOne({ email });
+
+  if (isEmailTaken) {
+    const error = new Error(`El correo ${email} ya está en uso.`);
+    return res.status(400).json({ msg: error.message });
+  }
+
+  try {
+    // Save new veterinarian
+    const veterinarian = new Veterinarian(req.body);
+    const savedVeterinarian = await veterinarian.save();
+    res.json(savedVeterinarian);
+  } catch (error) {
+    console.error(error);
+  }
 };
