eb5c63761896d0b91562b179b548b15545879739
Carlos <carlosaac232001@gmail.com>
2026-01-24 12:24:50 -0500

    feat: update index file with handle routing

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-import express from 'express';
+import dotenv from 'dotenv';
+import e from 'express';
 
-const app = express();
+import { connectDB } from './config/db.js';
+import veterinarianRoutes from './routes/veterinarianRoutes.js';
+
+const app = e();
+app.use(e.json());
+dotenv.config();
+connectDB();
+
+app.use('/api/veterinarians', veterinarianRoutes);
+
+const PORT = process.env.PORT || 4000;
 
 app.listen(4000, () => {
-  console.log('Servidor funcionando en el puerto 4000');
+  console.log(`Servidor funcionando en el puerto ${PORT}`);
 });
