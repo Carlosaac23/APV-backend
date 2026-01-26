eed26f1445319a89eae41939bf0ade6e6ef0c478
Carlos <carlosaac232001@gmail.com>
2026-01-25 19:57:55 -0500

    chore: create new routes to recover and change password

--- routes/veterinarianRoutes.js
+++ routes/veterinarianRoutes.js
@@ -4,6 +4,9 @@ import {
   registerVeterinarian,
   confirmAccount,
   authenticateVeterinarian,
+  forgotPassword,
+  verifyToken,
+  newPassword,
   accountProfile,
 } from '../controllers/veterinarianController.js';
 import { checkAuth } from '../middleware/authMiddleware.js';
@@ -12,6 +15,8 @@ import { checkAuth } from '../middleware/authMiddleware.js';
 router.post('/', registerVeterinarian);
 router.get('/confirm/:token', confirmAccount);
 router.post('/login', authenticateVeterinarian);
+router.post('/forgot-password', forgotPassword);
+router.route('/forgot-password/:token').get(verifyToken).post(newPassword);
 
 // Private routes
 router.get('/profile', checkAuth, accountProfile);
