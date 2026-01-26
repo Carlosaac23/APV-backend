13b0ee33bb2c61f77d3b1fe7515fe0411f20672c
Carlos <carlosaac232001@gmail.com>
2026-01-25 20:17:10 -0500

    chore: import new functions names

--- routes/veterinarianRoutes.js
+++ routes/veterinarianRoutes.js
@@ -2,23 +2,26 @@ import e from 'express';
 const router = e.Router();
 import {
   registerVeterinarian,
-  confirmAccount,
+  confirmVeterinarianAccount,
   authenticateVeterinarian,
   forgotPassword,
-  verifyToken,
-  newPassword,
-  accountProfile,
+  resetPasswordToken,
+  resetPassword,
+  getVeterinarianProfile,
 } from '../controllers/veterinarianController.js';
 import { checkAuth } from '../middleware/authMiddleware.js';
 
 // Public routes
 router.post('/', registerVeterinarian);
-router.get('/confirm/:token', confirmAccount);
+router.get('/confirm/:token', confirmVeterinarianAccount);
 router.post('/login', authenticateVeterinarian);
 router.post('/forgot-password', forgotPassword);
-router.route('/forgot-password/:token').get(verifyToken).post(newPassword);
+router
+  .route('/forgot-password/:token')
+  .get(resetPasswordToken)
+  .post(resetPassword);
 
 // Private routes
-router.get('/profile', checkAuth, accountProfile);
+router.get('/profile', checkAuth, getVeterinarianProfile);
 
 export default router;
