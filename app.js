de492e2f0090db623ca6224d6a4ac24b8d9fccf1
Carlos <carlosaac232001@gmail.com>
2026-02-04 14:00:01 -0500

    chore: rename main js file to app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 import dotenv from 'dotenv';
-import e from 'express';
+import express from 'express';
 
 import { connectDB } from './config/db.js';
 import veterinarianRoutes from './routes/veterinarianRoutes.js';
 
-const app = e();
-app.use(e.json());
+const app = express();
+app.use(express.json());
 dotenv.config();
 connectDB();
 
@@ -14,5 +14,5 @@ app.use('/api/veterinarians', veterinarianRoutes);
 const PORT = process.env.PORT || 4000;
 
 app.listen(4000, () => {
-  console.log(`Servidor funcionando en el puerto ${PORT}`);
+  console.log(`Server working on http://localhost:${PORT}`);
 });
