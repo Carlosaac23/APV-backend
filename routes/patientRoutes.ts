f3771f34d0c5195d633df628eacfdfb99f64294f
Carlos <carlosaac232001@gmail.com>
2026-03-04 18:04:35 -0500

    feat: change routes file from js to ts

---
+++ routes/patientRoutes.ts
@@ -0,0 +1,22 @@
+import { Router } from 'express';
+
+import {
+  addPatient,
+  getPatients,
+  getPatientById,
+  updatePatientById,
+  deletePatientById,
+} from '../controllers/patientControllers.js';
+import { checkAuth } from '../middleware/authMiddleware.js';
+
+export const patientRoutes: Router = Router();
+
+patientRoutes
+  .route('/')
+  .post(checkAuth, addPatient)
+  .get(checkAuth, getPatients);
+patientRoutes
+  .route('/:patientID')
+  .get(checkAuth, getPatientById)
+  .put(checkAuth, updatePatientById)
+  .delete(checkAuth, deletePatientById);
