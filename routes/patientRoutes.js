df3f594d81b08b77a4bd2b2fcf1b8aeabdc36459
Carlos <carlosaac232001@gmail.com>
2026-03-04 11:45:22 -0500

    chore: coment code for refactor

--- routes/patientRoutes.js
+++ routes/patientRoutes.js
@@ -1,21 +1,22 @@
-import { Router } from 'express';
+// import { Router } from 'express';
 
-import {
-  addPatient,
-  getPatients,
-  getPatientById,
-  updatePatientById,
-  deletePatientById,
-} from '../controllers/patientControllers.js';
-import { checkAuth } from '../middleware/authMiddleware.js';
+// import {
+//   addPatient,
+//   getPatients,
+//   getPatientById,
+//   updatePatientById,
+//   deletePatientById,
+// } from '../controllers/patientControllers.js';
+// import { checkAuth } from '../middleware/authMiddleware.js';
 
-const router = Router();
+// export const patientRoutes = Router();
 
-router.route('/').post(checkAuth, addPatient).get(checkAuth, getPatients);
-router
-  .route('/:patientID')
-  .get(checkAuth, getPatientById)
-  .put(checkAuth, updatePatientById)
-  .delete(checkAuth, deletePatientById);
-
-export default router;
+// patientRoutes
+//   .route('/')
+//   .post(checkAuth, addPatient)
+//   .get(checkAuth, getPatients);
+// patientRoutes
+//   .route('/:patientID')
+//   .get(checkAuth, getPatientById)
+//   .put(checkAuth, updatePatientById)
+//   .delete(checkAuth, deletePatientById);
